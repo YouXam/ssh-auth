7e78f6bfbf04bd79f5689ca99c4064daed125ae7
YouXam <zhang.haoling@outlook.com>
2022-10-24 20:22:29 +0800

    Add "show" function and fix some bug.

--- src/go.mod
+++ src/go.mod
@@ -4,6 +4,7 @@ go 1.19
 
 require (
 	github.com/howeyc/gopass v0.0.0-20210920133722-c8aef6fb66ef
+	github.com/liushuochen/gotable v0.0.0-20220831134725-cbcd6bb0a5f9
 	github.com/mattn/go-sqlite3 v1.14.15
 	github.com/pkg/sftp v1.13.5
 	golang.org/x/crypto v0.1.0
--- src/server.go
+++ src/server.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"github.com/liushuochen/gotable"
+	"strconv"
 )
 
 func addServer(destination string, port int, usePassword bool, publicKeyPath string, serverName string) {
@@ -9,8 +11,22 @@ func addServer(destination string, port int, usePassword bool, publicKeyPath str
 	fmt.Println("Testing ssh connection...")
 	connect(r, true)
 	if insertServer(r.hostname, r.port, r.username, serverName, r.password, r.privateKey) {
-		fmt.Printf("Successfully added server %s.\n", r.toString())
+		fmt.Printf("Successfully added server %v.\n", r)
 	} else {
-		fmt.Printf("Successfully modified server %s.\n", r.toString())
+		fmt.Printf("Successfully modified server %v.\n", r)
 	}
+}
+
+func showServer() {
+	servers := getServers()
+	table, _ := gotable.Create("ID", "Name", "Address", "Type")
+	for _, e := range servers {
+		ct := "password"
+		if e.privateKey != "" {
+			ct = "private key"
+		}
+		_ = table.AddRow([]string{strconv.Itoa(e.id), e.servername, e.username + "@" + e.hostname + ":" + strconv.Itoa(e.port), ct})
+	}
+	fmt.Print(table)
+
 }
